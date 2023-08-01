d195b5cdbbb11cb7a163416597dc644e2bfbcacf
Dibague <nachodbartolo@gmail.com>
2023-07-31 21:16:16 -0300

    Printea todas las comisionesy sus horarios de la materia inputeada por el user

--- scrapper.py
+++ scrapper.py
@@ -1,21 +1,26 @@
 
 from bs4 import BeautifulSoup
 
-with open("hrhs.html", "r", encoding="UTF-8") as html_file:
+x = input("Ingrese el codigo de la materia: ")
+
+with open("siu.html", "r", encoding="UTF-8") as html_file:
     content = html_file.read()
 
     soup = BeautifulSoup(content, "lxml")
     
-    materia = soup.find('div', attrs={"actividad" : "Álgebra y Geometría (GMA0126)"})
+    materia = soup.find('div', attrs={"actividad" : x})
 
     clases = materia.find_all("table", attrs={"sede" : "1"})
     for comision in clases:
         codigo = comision.find("h5")
-        infocom = comision.find("tr", class_="js-dia")
-        tipo = infocom.select("td")[0].text.replace("\n", "").replace("\t", "").replace(" ", "")
-        dia = infocom.select("td")[1].text.replace("\n", "").replace("\t", "").replace(" ", "")
-        hora = infocom.select("td")[2].text.replace("\n", "").replace("\t", "").replace(" ", "")
-        print(codigo.text,"\n\t", tipo, dia, hora, "\n\n")
+        infocom = comision.find_all("tr", class_="js-dia")
+        #print(infocom)
+        print(codigo)
+        for casilla in infocom:
+            tipo = casilla.select("td")[0].text.replace("\n", "").replace("\t", "").replace(" ", "")
+            dia = casilla.select("td")[1].text.replace("\n", "").replace("\t", "").replace(" ", "")
+            hora = casilla.select("td")[2].text.replace("\n", "").replace("\t", "").replace(" ", "")
+            print("\n\t", tipo, dia, hora, "\n\n")
         
         
         
