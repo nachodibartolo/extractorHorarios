258704d9a4764fc7ea2dadacc06fa3c96d1546d0
Dibague <nachodbartolo@gmail.com>
2023-08-01 12:09:02 -0300

    Retorna la lista ordenada de las materias sin comsiones repetidas

--- scrapper.py
+++ scrapper.py
@@ -1,38 +1,57 @@
-
 from bs4 import BeautifulSoup
+import os
 
-x = input("Ingrese el codigo de la materia: ")
 
-with open("siu.html", "r", encoding="UTF-8") as html_file:
-    content = html_file.read()
+def limpiarArch():
+    lines_seen = set() # holds lines already seen
+    outfile = open("tempord.txt", "w")
+    for line in open("tempdes.txt", "r"):
+        if line not in lines_seen: # not a duplicate
+            outfile.write(line)
+            lines_seen.add(line)
+    os.remove("tempdes.txt")
+    outfile.close()
 
-    soup = BeautifulSoup(content, "lxml")
+
+def guardar(f, n):
+    for i in range(int(n)):
+        nombreMat = input("Ingrese el codigo de la materia: ")
+        with open("siu.html", "r", encoding="UTF-8") as html_file:
+            content = html_file.read()
+        soup = BeautifulSoup(content, "lxml")
+
+        materia = soup.find('div', attrs={"actividad" : nombreMat})
+        clases = materia.find_all("table", attrs={"sede" : "1"})
+        print("\n\n", nombreMat, "\n")
+        c=0
+        for comision in clases:
+            codigo = comision.find("h5")
+            infocom = comision.find_all("tr", class_="js-dia")
+            print(codigo.text)
+
+            for casilla in infocom:
+
+                tipo = casilla.select("td")[0].text.replace("\n", "").replace("\t", "").replace(" ", "")
+                dia = casilla.select("td")[1].text.replace("\n", "").replace("\t", "").replace(" ", "")
+                hora = casilla.select("td")[2].text.replace("\n", "").replace("\t", "").replace(" ", "").replace("a", ",").replace(":", ".")
+
+                print("\n\t", tipo, dia, hora, "\n\n")
+                f.write(" " + nombreMat + ";" + codigo.text + ";" + tipo + "," + dia + "," + hora + "\n")
+            c+=1
     
-    materia = soup.find('div', attrs={"actividad" : x})
 
-    clases = materia.find_all("table", attrs={"sede" : "1"})
-    for comision in clases:
-        codigo = comision.find("h5")
-        infocom = comision.find_all("tr", class_="js-dia")
-        #print(infocom)
-        print(codigo)
-        for casilla in infocom:
-            tipo = casilla.select("td")[0].text.replace("\n", "").replace("\t", "").replace(" ", "")
-            dia = casilla.select("td")[1].text.replace("\n", "").replace("\t", "").replace(" ", "")
-            hora = casilla.select("td")[2].text.replace("\n", "").replace("\t", "").replace(" ", "")
-            print("\n\t", tipo, dia, hora, "\n\n")
+
         
-        
-        
-        #print(codigo,infocom)
-        #print("--------------------------------------------------")
+            
+def main():
 
-    
-    
-    
-    
-    
-    #for materia in materias:
-        #sede = materia.find("table", attrs={"sede" : "1"})
-            #for row in comision:
-                #row = comision.find("td").text
+    n = input("Ingrese la cantidad de materias: ")
+    matriz = [["" for i in range(int(n))] for i in range(int(n))]
+    f = open("tempdes.txt", "w")
+    guardar(f, n)
+    f.close()
+    limpiarArch()
+
+
+
+main()
